8c0c8d50b95dd2246b31364d3063df5d23d7902c
jamesbrown1417 <james.brown1417@gmail.com>
2024-07-26 22:02:49 +0930

    automated commit and timestamp 2024-07-26 22:02:49

--- Apps/SGM/.shiny_run/shinyShortcut.cmd
+++ Apps/SGM/.shiny_run/shinyShortcut.cmd
@@ -1,1 +1,1 @@
-"C:\PROGRA~1\R\R-43~1.3\bin\Rscript.exe" -e "shiny::runApp('C:/Users/james/R_Projects/AFL-2024/Apps/SGM', launch.browser = TRUE)"
+"C:\PROGRA~1\R\R-44~1.1\bin\Rscript.exe" -e "shiny::runApp('C:/Users/james/OneDrive/Desktop/Projects/AFL-2024/Apps/SGM', launch.browser = TRUE)"
