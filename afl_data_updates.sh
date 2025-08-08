4d2db802f2e917ccb30ae31f4e9eb21ea981ed63
James <james.brown1417@gmail.com>
2025-08-08 11:05:21 +0930

    automated commit and timestamp 2025-08-08 11:05:21

--- afl_data_updates.sh
+++ afl_data_updates.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
+# Give access to normal path variables
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
 # Set the current directory to your project folder
 cd /Users/jamesbrown/Projects/AFL || exit
 
 # Execute R scripts in sequence
-Rscript Scripts/get_dvp.R
+Rscript Scripts/get_current_fantasy.R
+Rscript DVP/get_dvp.R
 Rscript Scripts/empirical_probs_last_n.R
 Rscript Scripts/empirical_probs_2025.R
-Rscript Scripts/get_current_fantasy.R
-
-# Automatically stage all changes
-git add .
-
-# Commit changes with a message including "automated commit" and the current timestamp
-commitMessage="automated commit: data updates $(date '+%Y-%m-%d %H:%M:%S')"
-git commit -m "$commitMessage"
-
-# Push the commit to the 'main' branch on 'origin'
-git push origin main
