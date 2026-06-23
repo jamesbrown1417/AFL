41cc311e542dc7dbbe2d57ecc0e6ed12479d2149
James <james.brown1417@gmail.com>
2026-06-23 16:04:17 +0930

    automated commit and timestamp 2026-06-23 16:04:17

--- afl_update_file.sh
+++ afl_update_file.sh
@@ -13,7 +13,9 @@ find OddsScraper/Neds -maxdepth 1 -name 'data_*.json' -delete
 find Data/BET365_HTML -maxdepth 1 -name '*.txt' -delete
 
 # Execute Python and R scripts
-/Users/jamesbrown/.pyenv/versions/3.12.5/bin/python3 OddsScraper/bet365_afl_single.py
+if ! /Users/jamesbrown/.pyenv/versions/3.12.5/bin/python3 OddsScraper/bet365_afl_single.py; then
+    echo "Bet365 scrape failed. Continuing with the remaining update steps."
+fi
 /Users/jamesbrown/.pyenv/versions/3.12.5/bin/python3 OddsScraper/TAB/get-TAB-response.py
 
 /Users/jamesbrown/.pyenv/versions/3.12.5/bin/python3 OddsScraper/Neds/get_neds_urls.py
